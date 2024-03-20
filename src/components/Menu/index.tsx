388e5b86f66517909dce3244bc43e35afdbf9a8b
Thiago Bruno <tbgt35@hotmail.com>
2024-03-19 23:32:24 -0300

    estilixando menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,13 +4,27 @@ import { styles } from "./styles";
 import { forwardRef } from "react";
 import { MenuProps } from "./menu";
 import { FontAwesome } from "@expo/vector-icons";
+import { theme } from "@/theme";
 
 export const Menu = forwardRef<BottomSheet, MenuProps>(({ onClose }, ref) => {
   return (
-    <BottomSheet ref={ref} index={0} snapPoints={[0.01, 230]}>
-      <View>
-        <FontAwesome name="close" size={24} onPress={onClose} />
-        <Text>Comece a criar agora</Text>
+    <BottomSheet
+      ref={ref}
+      index={0}
+      snapPoints={[0.01, 230]}
+      backgroundStyle={styles.container}
+      handleComponent={() => null}
+    >
+      <View style={styles.content}>
+        <View style={styles.header}>
+          <FontAwesome
+            name="close"
+            size={24}
+            color={theme.colors.white}
+            onPress={onClose}
+          />
+          <Text style={styles.title}>Comece a criar agora</Text>
+        </View>
       </View>
     </BottomSheet>
   );
